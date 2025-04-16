841bced56430b140eb3ce8a49af873dadfd2eba1
Helgi Espel <helgiespel@gmail.com>
2025-04-16 19:42:21 +0000

    Added setup method to dice test class with before annotation

--- src/test/java/hi/verkefni/verkefni3/vinnsla/TeningurTest.java
+++ src/test/java/hi/verkefni/verkefni3/vinnsla/TeningurTest.java
@@ -5,10 +5,18 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+
+
 public class TeningurTest {
+
+    private Teningur dice;
+
+    @Before
+    public void setUp() {
+        dice = new Teningur();
+    }
     @Test
     public void testRoll() {
-        Teningur dice = new Teningur();
         dice.roll();
         int diceNumber = dice.getDiceNumber();
         assertTrue(diceNumber >= 1 && diceNumber <= 6);
@@ -16,12 +24,12 @@ public class TeningurTest {
 
     @Test
     public void testPropertyMatchesNumber() {
-        Teningur dice = new Teningur();
         dice.roll();
         int diceNumber = dice.diceNumber;
         int diceProperty = dice.diceProperty().get();
         assertEquals(diceNumber, diceProperty);
     }
+
 
 
 }
